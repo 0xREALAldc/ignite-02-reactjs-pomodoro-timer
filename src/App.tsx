0aac6afc068cbad8b545a82b277d848761ffe662
0xREALaldc <andredonat@hotmail.com>
2023-04-10 18:03:34 -0300

    Add: Initial changes to add routing with pages in our application

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>teste</h1>
+      {/* we need to have the BrowserRouter around the Router that has our routes */}
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
